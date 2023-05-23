db41cdf3dfba52ab895e7f8fbbc151e798abb42c
Bobby Habtamu <bobby.habtamu@gmail.com>
2023-05-22 23:09:28 -0700

    Adds comments

--- button.py
+++ button.py
@@ -1,17 +1,17 @@
-# 
+# Calling and naming the class library
 import tkinter as tk
 
-#
+#creating the print function
 def button_click():
     print("Button clicked!")
     
-#
+# Title on the root window
 root = tk.Tk()
 root.title("Button Example")
 
-#
+# Creating the button function 
 button = tk.Button(root,text="Click Me!", command=button_click)
 button.pack()
 
-#
+# Looping the program indefinetly 
 root.mainloop()
--- combo_box.py
+++ combo_box.py
@@ -1,14 +1,14 @@
-#
+#Calling and naming the class library
 import tkinter as tk
 from tkinter import ttk
 
 
-#
+#Setting the argument parameter 
 def on_select(event):
     
     selected_item = event.widget.get()
     print("Selected item:", selected_item)
     
-#
+#Title for the root window
 root = tk.Tk()
 root.title("Combobox Example")
